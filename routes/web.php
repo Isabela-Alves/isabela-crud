9a7f59cf1fb1fc871529264ff0ce27ad81ffc5b1
Isabela Alves <108693206+Isabela-Alves@users.noreply.github.com>
2023-05-06 18:51:37 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     
-    Route::resource('/material',MaterialController::class)->except(['index']);
+    Route::resource('/material',MaterialController::class)->except(['index' , 'edit']);
 });
 
 require __DIR__.'/auth.php';
